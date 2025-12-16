283a4c8ad798f453c017a73502032f15d9e6b84c
barrientosverab <barrientosverab@gmail.com>
2025-12-16 13:31:49 -0400

    feat: correccion doble sidebar 1

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { Sidebar } from './Sidebar'; // Asegúrate de que Sidebar ya sea .tsx
 import { SubscriptionGuard } from '../auth/SubscriptionGuard';
 
 export const Layout = () => {
   return (
     <div className="flex h-screen w-full bg-stone-50 overflow-hidden">
-      {/* Barra Lateral */}
-      <Sidebar />
-
-      {/* Contenido Principal */}
       <main className="flex-1 h-full overflow-y-auto relative flex flex-col">
         {/* Outlet renderiza la página hija correspondiente (Dashboard, Ventas, etc.) */}
                       <SubscriptionGuard><Outlet/></SubscriptionGuard>
